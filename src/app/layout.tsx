69c4e0aaa9ad3a5fbfe1c9711d97dcf6ee7249d5
Muhammad Rico Ardiansyah <115982526+0xricoard@users.noreply.github.com>
2026-01-20 08:40:08 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        {/* Google AdSense */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9917389238286564"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={`${inter.variable} font-sans antialiased`}>
         <ThemeProvider
           defaultTheme="system"
